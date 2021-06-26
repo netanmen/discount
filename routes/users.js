e1d67a9977c19dc9938daf33426b87226fdb7c74
Netanel Mendel <netanel.mendel@storrsoft.com>
2021-06-26 14:20:38 +0300

    create and delete user endpoints

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,15 @@ const router = express.Router();
 const fs = require('fs').promises;  
 const users = require('../data/users.json');
 
+const usersPath = './data/users.json';
+
 router.get('/', function (req, res, next) {
   res.send(users);
 });
 
 router.get('/:id', function (req, res, next) {
   const { id } = req.params;
+  
   const user = users.find(user => user.customerID === id);
 
   res.send(user);
@@ -16,6 +19,7 @@ router.get('/:id', function (req, res, next) {
 
 router.get('/:id', function (req, res, next) {
   const { id } = req.params;
+  
   const user = users.find(user => user.customerID === id);
 
   res.send(user);
@@ -23,11 +27,20 @@ router.get('/:id', function (req, res, next) {
 
 router.delete('/:id', async (req, res, next) => {
   const { id } = req.params;
-  const user = users.filter(user => user.customerID !== id);
-  await fs.writeFile('../data/users.json', 'test');
   
-
+  const updatedUsers = users.filter(user => user.customerID !== id);
+  await fs.writeFile(usersPath, JSON.stringify(updatedUsers, null, 4));
+  
   res.sendStatus(200);
+});
+
+router.post('/', async (req, res, next) => {
+  const { body: newUser } = req;
+  
+  users.push(newUser);
+  await fs.writeFile(usersPath, JSON.stringify(users, null, 4));
+  
+  res.sendStatus(201);
 });
 
 module.exports = router;
